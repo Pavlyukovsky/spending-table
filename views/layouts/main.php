a80f1b10be5a3988e2393ed04f340372085777ba
Dima Pavlyuk <pro10097Sasha@gmail.com>
2017-01-16 00:14:55 +0200

    fixed layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,20 +37,9 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Bill List', 'url' => ['/bill/bill']],
+            ['label' => 'Bill Category', 'url' => ['/bill/category']],
+            ['label' => 'Bill Test', 'url' => ['/bill/bill/test']],
         ],
     ]);
     NavBar::end();
